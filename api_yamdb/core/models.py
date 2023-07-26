613935919b90575b143dae1f0f9e79fc8242725e
Стас Басов~ <stas.basov@whatever.com>
2023-07-26 20:21:21 +0500

    Добавленны модели Review, Comment

--- api_yamdb/core/models.py
+++ api_yamdb/core/models.py
@@ -22,7 +22,49 @@ class User(AbstractUser):
 
 
 class Review(models.Model):
-    ...
+    """Отзывы."""
+    title = models.ForeignKey(
+        Title,
+        verbose_name="Произведение",
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        null=True
+    )
+    text = models.TextField(
+        verbose_name="Текст",
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name="Автор",
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name="Рейтинг",
+        validators=[
+            MinValueValidator(1, "Введенная оценка ниже допустимой"),
+            MaxValueValidator(10, "Введенная оценка выше допустимой")
+        ]
+    )
+    pub_date = models.DateTimeField(
+        verbose_name="Дата публикации",
+        auto_now_add=True,
+        db_index=True
+    )
+    
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+        ordering = ('-pub_date',)
+        constraints = (
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name="unique_author_title"
+            ),
+        )
+    
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
@@ -52,6 +94,38 @@ class Title(models.Model):
         null=True,
     )
     genre = models.ManyToManyField(Genre, through="GenreTitle")
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    """Комментарии."""
+    review = models.ForeignKey(
+        Review,
+        verbose_name="Отзыв",
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    text = models.TextField(
+        verbose_name="Текст",
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    pub_date = models.DateTimeField(
+        verbose_name="Дата публикации",
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ['pub_date']
 
     def __str__(self):
         return self.name
