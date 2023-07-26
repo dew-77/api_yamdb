77dafff0b6a4ab6f5ddfe7aef70bd1e3a2aee67f
Андрей Богидаев <andrey.bogidaev03@gmail.com>
2023-07-26 17:08:03 +0300

    Добавлены модели Title, Category, Genre

--- api_yamdb/core/models.py
+++ api_yamdb/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.TextField("Название")
+    slug = models.SlugField("Краткое название", unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.TextField("Название")
+    slug = models.SlugField("Краткое название", unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField("Название")
+    year = models.DateTimeField("Год выпуска", auto_now_add=True)
+    category = models.ForeignKey(
+        Category,
+        verbose_name="Произведения",
+        related_name="titles",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    genre = models.ManyToManyField(Genre, through="GenreTitle")
+
+    def __str__(self):
+        return self.name
