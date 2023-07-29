c799a482dbf5dbfb7135961040b68fbb82d60ee0
Artem Yurkov <yurkov.artyom21@gmail.com>
2023-07-29 16:16:36 +0300

    Добавлено разрешение для проверки авторства

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -13,3 +13,24 @@ class OnlyAdmin(BasePermission):
 class ReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        if request.user.is_authenticated:
+            if (request.user.role == "admin"
+                or request.user.role == "moderator"
+                    or request.user.is_superuser):
+                return True
+
+            if obj.author == request.user:
+                return True
+
+        return False
