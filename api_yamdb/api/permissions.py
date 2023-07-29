34484760fdd5d55ea49865d1f419f754a7009f3d
Artem Yurkov <yurkov.artyom21@gmail.com>
2023-07-29 17:06:27 +0300

    Добавлены разрешения для модератора

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,18 +1,18 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class OnlyAdmin(BasePermission):
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+
+class IsAdmin(BasePermission):
     """Право доступа только для пользователей c ролью 'admin'."""
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
-            return request.user.is_superuser or request.user.role == "admin"
-        return False
-
-
-class ReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
+            return (request.user.is_authenticated and (
+                request.user.role == "admin" or request.user.is_superuser))
 
 
 class IsAuthorOrReadOnly(BasePermission):
@@ -24,13 +24,22 @@ class IsAuthorOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
 
-        if request.user.is_authenticated:
-            if (request.user.role == "admin"
-                or request.user.role == "moderator"
-                    or request.user.is_superuser):
-                return True
+        if request.user.is_authenticated and obj.author == request.user:
+            return True
 
-            if obj.author == request.user:
-                return True
+        return request.user.is_superuser
 
-        return False
+
+class IsModeratorOrAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.user.is_authenticated
+            and (
+                request.user.role == "moderator"
+                or request.user.role == "admin"
+                or request.user.is_superuser)
+        )
